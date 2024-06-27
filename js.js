285abb09ca84572516fb2e895b7f10f8903ee8b6
EverestEgg <142887211+EverestEgg@users.noreply.github.com>
2024-06-27 14:40:02 +1000

    Add files via upload

--- js.js
+++ js.js
@@ -1,20 +1,32 @@
 
 $(document).ready(function(){
     
-  let storyMark = 0
-  var checkText = $('input:text').val()
+  //stoy vaiabules
+  let storyMark;
+  storyMark = 0;
+  let location = 'land';
+
+  //code vaibuls
+  function resetText() {$('input').val('>');} //add focase at the end once done
+  function wrongInp() {
+    $('#wrong').show();
+    resetText();
+    $('#wrong').fadeOut(2000);} //show popup with wrong imput
+
+
 
    $('#hintPop').hide()
+   $('#wrong').hide()
     //remove when done
-    //$("#open").hide();
-   //$("#start").hide();
+    $("#open").hide();
+    $("#start").hide();
+    resetText();
 
    //Remove starting screen
   $("#start").click(function(){
     $("#open").fadeOut(3000);
     $("#start").fadeOut(400);
-    $('input').focus();
-    $('input:text').val('>')
+    resetText();
   });
 
     //click hint Button 
@@ -29,18 +41,33 @@ $(document).ready(function(){
 
   //on press enter key
   $(document).on('keypress',function(e) {
-
     if(e.which == 13) {
-    
-      $('input:text').val()
-      $('input:text').val('>')
+      console.log($('input').val())
 
-      if(storyMark == 0) {
-        $('p').text('hears your typing saskia');
-        storyMark += 1
-      } else {
-        $('p').text('no more');
+      switch(storyMark){
+        case 0:
+          //see if input matchs story
+          if($('input').val() == '>hi' ) {
+          $('p').text('story progress 1');
+          storyMark = 1;
+          resetText();
+          } else { wrongInp();}
+          break
+      
+        case 1:
+          if($('input').val() == '>go' ) {
+            $('p').text('story progress 2');
+            storyMark = 2;
+            resetText();
+            } else if ($('input').val() == '>stop') {
+              $('p').text('story progress diff');
+             storyMark = 3;
+             resetText();
+            } else { wrongInp(); }
+          break
+
       }
+     
     }
 
    });
