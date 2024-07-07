4fd5c78ac8b8b6f282d22f838097a3c6e5b082ee
EverestEgg <142887211+EverestEgg@users.noreply.github.com>
2024-07-07 15:16:36 +1000

    Add files via upload

--- js.js
+++ js.js
@@ -1,17 +1,37 @@
 
+
 $(document).ready(function(){
+  
+  //
+  var showText = function(target, message, index, interval) {
+    if (index < message.length) {
+      $(target).append(message[index++]);
+      setTimeout(function() {
+        showText(target, message, index, interval);
+      }, interval);
+    }
+  }
+ $(function() {
+    showText("#responce",'', '', '');
+  });
     
   //stoy vaiabules
   let storyMark;
   storyMark = 0;
-  let location = 'land';
+  //let location = 'land';
 
   //code vaibuls
-  function resetText() {$('input').val('>').focus();} //add focase at the end once done
+  function resetText() {
+    $('input').val('>').focus(); 
+  } //add focase at the end once done
+  
   function wrongInp() {
-    $('#wrong').show().fadeOut(2000);
-    resetText();} //show popup with wrong imput
-
+    $('#wrong').show();
+    showText('#wrong',"Unrecongnised Input",0,50)
+    $('#wrong').fadeOut(3500)
+    resetText();
+    $('#wrong').text('U');
+  } //show popup with wrong imput
 
 
    $('#hintPop').hide()
@@ -25,6 +45,7 @@ $(document).ready(function(){
   $("#start").click(function(){
     $("#open").fadeOut(3000);
     $("#start").fadeOut(800);
+    showText("#responce", "The Ocean stretches out before you, seemingly endlessly though you know that is not true. In front of you sits your future home, bobbing calmly in the waves. To your left your two crewmates that you will spend the next few months with, Synthia and _____. Directly in front of you is _____, your main contact to civilization for the mission, they are speaking now", 0, 22);
     resetText();
   });
 
@@ -46,20 +67,22 @@ $(document).ready(function(){
       switch(storyMark){
         case 0:
           //see if input matchs story
-          if($('input').val() == '>hi' ) {
-          $('p').text('story progress 1');
+          if($('input').val() == '>next' || ">Next" ||'>n'||'>N'||'n'||'N') {
+            $('p').text('')
+            showText("#responce", 'Focus up LandCon says. ‘We are sending the three of you to explore territory that has been inaccessible until now, The depths of the ocean. This specially built Submarine “The (greek myth name)” and deep sea suits have been developed to survive depths of up to (number km). You are our mission leader, pilot and communications expert, Synthia is our biologist, going out to document new sealife and plants. ____ is our mechanic, and geologist documenting the landscape of the ocean floor.', 0, 25)
+          typeWriter();
           storyMark = 1;
           resetText();
           } else { wrongInp();}
           break
       
         case 1:
-          if($('input').val() == '>go' ) {
-            $('p').text('story progress 2');
+          if($('input').val() == '>next' || ">Next" ||'>n'||'>N'||'n'||'N' ) {
+            showText("#responce", 'No More has been Written', 0, 50)
             storyMark = 2;
             resetText();
-            } else if ($('input').val() == '>stop') {
-              $('p').text('story progress diff');
+            } else if ($('input').val() == '>stop' || ">Stop" ||'>s'||'>S'||'s'||'S') {
+              showText("#responce", 'All done (for now)', 0, 50)
              storyMark = 3;
              resetText();
             } else { wrongInp(); }
